375bc9a4a8c944b973e1ada53e282b2ac75acba7
ArnavBarik <71958007+ArnavBarik@users.noreply.github.com>
2021-03-24 16:42:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
 function setup(){
     canvas=createCanvas(400,300);
     canvas.center();
+
+    video=createCapture(VIDEO);
+    video.size(500,500);
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function modelLoaded(){
+    console.log("Model is Loaded!");
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+    }
 }
